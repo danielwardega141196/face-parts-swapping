517e439a95a31b3a5574694e950cf7bf2f61637a
Daniel Wardega <danielwardega141196@gmail.com>
2019-04-29 12:14:44 +0200

    Added a new file named 'modalOptions.js'(./blog/static/js/modalOptions.js). This file contains a class named 'ModalOptions'. The class is responsible for the behaviour, appearance and details of the modal options. Added a new file 'popper.min.js.map'(./blog/static/js/popper.min.js.map).

---
+++ blog/static/js/modalOptions.js
@@ -0,0 +1,85 @@
+/**
+ * Class which determines the behaviour, appearance and details of the modal options.
+ * Currently, the modal options are: '$modalUpperOption' and '$modalLowerOption'.
+ */
+class ModalOptions {
+
+    /**
+     * This function looks for the siblings(by using the jQuery method - https://api.jquery.com/siblings/)
+     * of the passed modal option '$modalOption'.
+     * To these of the siblings, which have the class 'KEY_OF_THE_SHINING_OBJECT',
+     * the class 'CLASS_OF_THE_BLOCKED_SHINING' is added.
+     * @param {jQuery} $modalOption - jQuery object representing a modal option
+     */
+    static blockShiningOfSiblings($modalOption) {
+        $modalOption.siblings().each(function () {
+            const $this = $(this);
+            if ($this.hasClass(KEY_OF_THE_SHINING_OBJECT)) {
+                $this.addClass(CLASS_OF_THE_BLOCKED_SHINING);
+            }
+        });
+    }
+
+    /**
+     * This function looks for the siblings(by using the jQuery method - https://api.jquery.com/siblings/)
+     * of the passed modal option '$modalOption'.
+     * From these of the siblings, which have the class 'KEY_OF_THE_SHINING_OBJECT',
+     * the class 'CLASS_OF_THE_BLOCKED_SHINING' is removed.
+     * @param {jQuery} $modalOption - jQuery object representing a modal option
+     */
+    static unblockShiningOfSiblings($modalOption) {
+        $modalOption.siblings().each(function () {
+            const $this = $(this);
+            if ($this.hasClass(KEY_OF_THE_SHINING_OBJECT)) {
+                $this.removeClass(CLASS_OF_THE_BLOCKED_SHINING);
+            }
+        });
+    }
+
+    /**
+     * This function unchecks all modal options.
+     * None of them looks as the chosen one.
+     * Currently, the modal options are: '$modalUpperOption' and '$modalLowerOption'.
+     */
+    static uncheckAllModalOptions() {
+        $modalUpperOption.removeClass(CLASS_OF_THE_CHOSEN_MODAL_OPTION);
+        ModalOptions.blockShiningOfSiblings($modalUpperOption);
+        $modalLowerOption.removeClass(CLASS_OF_THE_CHOSEN_MODAL_OPTION);
+        ModalOptions.blockShiningOfSiblings($modalLowerOption);
+    }
+
+    /**
+     * This function distinguishes the passed modal option '$modalOption'.
+     * This option looks as the chosen one.
+     * @param {jQuery} $modalOption - jQuery object representing a modal option
+     */
+    static distinguishModalOption($modalOption) {
+        $modalOption.addClass(CLASS_OF_THE_CHOSEN_MODAL_OPTION);
+        ModalOptions.unblockShiningOfSiblings($modalOption);
+    }
+
+    /**
+     * This function assigns 'photoIndex'(passed as a parameter)
+     * as the value for the key 'KEY_OF_THE_INDEX_OF_A_PHOTO'
+     * (the dictionary associated with the '$modalOption' object).
+     * @param {jQuery} $modalOption - jQuery object representing a modal option
+     * @param {number} photoIndex - an integer representing the value of the key 'KEY_OF_THE_INDEX_OF_A_PHOTO'
+     * in the dictionary associated with the '$modalOption' object.
+     */
+    static setIndexOfModalOption($modalOption, photoIndex) {
+        $modalOption.data(KEY_OF_THE_INDEX_OF_A_PHOTO, photoIndex);
+    }
+
+    /**
+     * This function sets the index('photoIndex') and the source('source')
+     * of the photo assigned to '$modalOption'.
+     * @param {jQuery} $modalOption - jQuery object representing a modal option
+     * @param {string} source - a value of the 'src' attribute in the photo assigned to '$modalOption'.
+     * @param {number} photoIndex - an integer representing the value of the key 'KEY_OF_THE_INDEX_OF_A_PHOTO'
+     * in the dictionary associated with the '$modalOption' object.
+     */
+    static setAttrAndData($modalOption, source, photoIndex) {
+        ModalOptions.setIndexOfModalOption($modalOption, photoIndex);
+        setImgSrc($modalOption, source);
+    }
+}
